6255fb23628264cdedd93f9ac33656298426f93b
MGomed <MKKurbandibirov@gmail.com>
2024-08-04 10:19:11 +0300

    Finished maps task

--- maps/maps.py
+++ maps/maps.py
@@ -13,7 +13,19 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        def get_rating(movie: dict) -> float:
+            rating = movie["rating_kinopoisk"]
+            countries = movie["country"]
+            if countries.count(",") >= 1 and rating != "":
+                return float(rating)
+
+        filtered_rating = map(get_rating, list_of_movies)
+        filtered_rating_list = [
+            rating for rating in filtered_rating if rating is not None and rating != 0
+        ]
+
+        return sum(filtered_rating_list) / len(filtered_rating_list)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +40,14 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        def filter_name(movie: dict) -> int:
+            movie_rating = movie["rating_kinopoisk"]
+            if movie_rating != "" and float(movie_rating) >= rating:
+                return movie["name"].count("и")
+
+            return 0
+
+        letters_count = map(filter_name, list_of_movies)
+
+        return sum(letters_count)
